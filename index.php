7c4f3b1bf567ffb89961a61303425e69d7a73f99
Bas van Dijk <bass_van_dijk@live.nl>
2014-11-21 18:12:53 +0100

    Another css update

--- index.php
+++ index.php
@@ -1,26 +1,41 @@
 <html>
 	<head>
 		<title>Gallery</title>
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
 		<link href="css/main.css" rel="stylesheet" type="text/css"/>
 		<link href="css/mobile.css" rel="stylesheet" type="text/css"/>
 		<link href="css/forms.css" rel="stylesheet" type="text/css"/>
+
 	</head>
-	<body>
+	<body id="background">
 		<div id="container">
 			<div id="header">
 				<div id="header-content">
-				<img src="media/icons/contact32-icon.png">Contact
+					<div class="header-logo">Gallery</div>
 				</div>
+			</div><br/><br/><br/><br/>
+			<!--<div id="content-div"><div id="content">-->
+			<div id="login-form">
+				<form action="" method="post">
+					<input type="text" name="username" id="login_form_username" placeholder="Username"/><br/>
+					<input type="password" name="password" id="login_form_username" placeholder="*******"/><br/>
+					<input type="submit" name="login_button" id="login_form_button" value="Login"/>
+				</form>	
 			</div>
-			<div id="content-div">
-				<div id="content">
-				Content
-				</div>
-			</div>
+			<!--</div></div>-->
 			<br/><br/>
 			<div id="footer">
 				<div id="footer-content">
-				Footer
+					<div class="footer-link-icons">
+						<div class="copy-right">Copyrights &copy <?php echo date("Y"); ?></div>
+						<table class="icon-tabel">
+							<tr>
+								<td class="td-footer-links"><img src="media/icons/facebook32-icon.png" style="width:75%;"/></td>
+								<td class="td-footer-links"><img src="media/icons/email32-icon.png"/></td>
+								<td class="td-footer-links"><img src="media/icons/twitter32-icon.png" style="width:80	%;"/></td>
+							</tr>	
+						</table>	
+					</div>	
 				</div>
 			</div>
 		</div>
