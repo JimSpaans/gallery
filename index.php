29fc7b45e6859172489aac7d1e7529cfc80c8ce2
BAS VAN DIJK <bass_van_dijk@live.nl>
2014-11-21 17:08:17 +0100

    First commit
    Basic commit, to get starting files uploaded.

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>Gallery</title>
+		<link href="css/main.css" rel="stylesheet" type="text/css"/>
+		<link href="css/mobile.css" rel="stylesheet" type="text/css"/>
+		<link href="css/forms.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<div id="header-content">
+				<img src="media/icons/contact32-icon.png">Contact
+				</div>
+			</div>
+			<div id="content-div">
+				<div id="content">
+				Content
+				</div>
+			</div>
+			<br/><br/>
+			<div id="footer">
+				<div id="footer-content">
+				Footer
+				</div>
+			</div>
+		</div>
+	</body>
+</html>	
