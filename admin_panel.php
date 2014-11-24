48d3e7c619561db28bcc8effa4f9475bb2937997
BAS VAN DIJK <bass_van_dijk@live.nl>
2014-11-24 17:32:40 +0100

    Update
    Which includes, visual updates, Background using Javascript.

---
+++ admin_panel.php
@@ -0,0 +1,13 @@
+<?php
+	include 'layout_inc/header.php';
+?>
+
+<div id="content-div">
+	<div id="content">
+	wtfwef
+	</div>	
+</div>
+
+<?php
+	include 'layout_inc/footer.php';
+?>
---
+++ layout_inc/header.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>Gallery</title>
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+		<link href="css/main.css" rel="stylesheet" type="text/css"/>
+		<link href="css/mobile.css" rel="stylesheet" type="text/css"/>
+		<link href="css/forms.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body id="background">
+		<div id="container">
+			<div id="header">
+				<div id="header-content">
+					<div class="header-logo">Gallery</div>
+				</div>
+			</div>
+			</div><br/><br/><br/><br/>
+			<div id="content-div">
+				<div id="content">	
